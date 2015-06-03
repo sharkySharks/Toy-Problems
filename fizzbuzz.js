b5f73c690b73b4a27afd67e8e47b41ebf0b4bb1d
Sharky <kaydenalthen@gmail.com>
2015-06-03 13:13:51 -0500

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+/* FIZZBUZZ */
+
+var fizzbuzz = function(number){
+    if(number % 3 === 0 && number % 5 === 0){
+        console.log("fizzbuzz");
+    } else if (number % 3 === 0){
+        console.log("fizz");
+    } else if (number % 5 === 0){
+        console.log("buzz");
+    } else {
+        console.log(number);
+    }
+}
